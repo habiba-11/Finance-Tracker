4285d0d32844d8ad15f9000feaf6019b652cf6b6
ranameligy <ranamelegy05@gail.com>
2025-11-23 21:59:09 +0200

    transaction

--- backend/src/transactions/transactions.controller.ts
+++ backend/src/transactions/transactions.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(
+    private readonly transactionsService: TransactionsService,
+  ) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.transactionsService.create(body);
+  }
+
+  // getUserTransactions() will be added by Member 4
+}
--- backend/src/transactions/transactions.service.ts
+++ backend/src/transactions/transactions.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  Transaction,
+  TransactionDocument,
+} from './schemas/transaction.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+  constructor(
+    @InjectModel(Transaction.name)
+    private transactionModel: Model<TransactionDocument>,
+  ) {}
+
+  async create(body: any) {
+    const { userId, amount, type, category, date, description } = body;
+
+    // ✅ Declarative validation
+    const required = ['userId', 'amount', 'type', 'category', 'date'];
+    const missing = required.filter((f) => !body[f]);
+    if (missing.length) {
+      throw new BadRequestException(
+       ` Missing fields: ${missing.join(', ')}`,
+      );
+    }
+
+    const transaction = new this.transactionModel({
+      userId,
+      amount,
+      type,
+      category,
+      date,
+      description,
+    });
+
+    // ✅ FR2: add transaction
+    return transaction.save();
+  }
+
+  // getUserTransactions() will be implemented by Member 4
+}
